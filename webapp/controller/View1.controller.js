ef0eafc4ba4ae17d4d4ba23b9654777492f9f14e
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-01-29 21:10:51 +0530

    Updated View and Controller

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,14 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-5_Aggregation-Binding.controller.View1", {
+			
+			onInit : function() {
+				var student = new sap.ui.model.json.JSONModel();
+				
+				student.loadData("model/studentData.json");
+			
+				this.getView().setModel(student);
+			}
 
 	});
 });
